b3bb45f42203e7009f9f74fafec95ba108b38bad
Haj B <phajib@gmail.com>
2019-06-01 02:00:36 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,10 @@
 class Application < Sinatra::Base
-  # Write your code here!
+  get '/' do
+    erb :index
+  end
 
+  # New route to respond to the form submission
+  post '/greet' do
+    erb :greet
+  end
 end
